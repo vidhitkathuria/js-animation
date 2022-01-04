c06f4a78ccc9c173e3cb37bff8ed460547c744ac
vidhitkathuria <72202024+vidhitkathuria@users.noreply.github.com>
2022-01-04 14:55:50 +0530

    added files

---
+++ app.js
@@ -0,0 +1,35 @@
+// gsap.fromTo(
+//   ".text",
+//   { y: 20, opacity: 0, duration: 1 },
+//   { y: 0, opacity: 1, duration: 1 }
+// );
+const tl = gsap.timeline({ defaults: { duration: 0.7, ease: "power1.out" } });
+tl.fromTo(
+  ".cookie-container",
+  { scale: 0 },
+  { scale: 1, ease: "elastic.out(1, 0.4)", duration: 1.5 }
+);
+tl.fromTo(
+  ".cookie",
+  { opacity: 0, x: -50, rotation: "-45deg" },
+  { opacity: 1, duration: 0.5, x: 0, rotation: "0deg" },
+  "<50%"
+);
+tl.fromTo(".text", { x: 30, opacity: 0 }, { x: 0, opacity: 1 }, "<");
+
+tl.fromTo(
+  ".cookie",
+  { y: 0, rotation: "0deg" },
+  { y: -20, yoyo: true, repeat: -1, rotation: "-20deg" }
+);
+tl.fromTo("#crumb", { y: 0 }, { y: -20, yoyo: true, repeat: -1 });
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  gsap.to(".cookie-container", {
+    opacity: 0,
+    y: 100,
+    duration: 0.75,
+    ease: "power1.out",
+  });
+});
